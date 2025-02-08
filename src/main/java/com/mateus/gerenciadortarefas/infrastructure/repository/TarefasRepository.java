f0934ecaca0b6d354def4da7958b2eaf2c9530d8
Mateus <tech.mateussilva@gmail.com>
2025-02-08 16:43:17 -0300

    Globalizando as Exceptions

--- src/main/java/com/mateus/gerenciadortarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/mateus/gerenciadortarefas/infrastructure/repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package com.mateus.gerenciadortarefas.infrastructure.repository;
 
 import com.mateus.gerenciadortarefas.infrastructure.entity.TarefasEntity;
+import com.mateus.gerenciadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.List;
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
 
-    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+    List<TarefasEntity> findByDataEventoBetweenAndStatus(LocalDateTime dataInicial,
+                                                         LocalDateTime dataFinal,
+                                                         StatusNotificacaoEnum status);
 
     List<TarefasEntity> findByEmailUsuario(String email);
 
